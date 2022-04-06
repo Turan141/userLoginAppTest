29af70e0ae124139e8b6aabfe02d83428a01d43b
Turan <90398677+Turan141@users.noreply.github.com>
2022-04-06 16:10:16 +0400

    minor changes, added input, styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,61 @@
 import React from "react";
-import styled, { css } from "styled-components";
-
 import { useForm, SubmitHandler } from "react-hook-form";
+import styled from "styled-components";
+
+const Section = styled.section`
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+`;
+
+const Form = styled.form`
+  position: relative;
+  padding: 25px;
+  margin: 10px;
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
+  align-content: flex-start;
+  align-items: flex-start;
+`;
+
+const Label = styled.label`
+  margin-top: 1rem;
+`;
 
 const Input = styled.input`
-  padding: 20px;
+  width: 40vw;
+  height: 5vh;
+  padding: 5px 5px 5px 15px;
+  background-color: #f3f3f3;
+  border: none;
+  margin-top: 5%;
+  display: flex;
+  flex-direction: column;
+  border-radius: 5px;
 `;
+
+const RememberMeDiv = styled.div`
+  width: 70%;
+`;
+
+const RememberCheckBox = styled.input`
+  border-radius: 1px solid black;
+  margin-top: 5%;
+`;
+
+const ConfirmInput = styled.input`
+  background-color: blue;
+  border: none;
+  width: 100%;
+  padding-top: 2vh;
+  padding-bottom: 2vh;
+  border-radius: 5px;
+  color: white;
+  margin-top: 5%;
+`;
+
+// Types
 
 type Inputs = {
   example: string;
@@ -24,17 +74,29 @@ export default function App() {
   console.log(watch("example")); // watch input value by passing the name of it
 
   return (
-    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("example")} />
+    <Section>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <Label>
+          Логин
+          <Input defaultValue="test" {...register("example")} />
+        </Label>
 
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("exampleRequired", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
+        {/* include validation with required or other standard HTML validation rules */}
 
-      <input type="submit" />
-    </form>
+        <Label>
+          Пароль
+          <Input {...register("exampleRequired", { required: true })} />
+        </Label>
+        {/* errors will return when field validation fails  */}
+        <RememberMeDiv>
+          <RememberCheckBox type="checkbox" />
+          <span style={{ marginLeft: "5%" }}>Запомнить пароль</span>
+        </RememberMeDiv>
+        {errors.exampleRequired && <span>This field is required</span>}
+
+        <ConfirmInput type="submit" />
+      </Form>
+    </Section>
   );
 }
